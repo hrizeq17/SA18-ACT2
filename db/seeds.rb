c1afd89b2c9413e7cbc9f0ebf8ee9de2679d78ef
hrizeq17 <hrizeq@memphis.edu>
2024-04-18 17:10:11 -0500

    done

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Product.delete_all
+Product.create(name: "Sample Product 1", price: 19.99, description: "iphone case")
+Product.create(name: "Sample Product 2", price: 29.99, description: "charger")
+Product.create(name: "Sample Product 3", price: 39.99, description: "headphones")
